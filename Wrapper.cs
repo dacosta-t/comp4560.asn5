3a2c9b2a604756ffc8bf8cda7e249c1e5f63ce2d
Eric Tsang <surplus.et@gmail.com>
2015-02-13 02:10:47 -0800

    initial commit
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ Wrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asgn5v1
+{
+        public class Wrapper<T> where T : struct
+        {
+            public static implicit operator T(Wrapper<T> w)
+            {
+                return w.Value;
+            }
+
+            public Wrapper(T t)
+            {
+                _t = t;
+            }
+
+            public T Value
+            {
+                get
+                {
+                    return _t;
+                }
+
+                set
+                {
+                    _t = value;
+                }
+            }
+
+            public override string ToString()
+            {
+                return _t.ToString();
+            }
+
+            private T _t;
+        } 
+}
